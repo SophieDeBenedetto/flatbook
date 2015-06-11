624426cf69833127b0fe0339c98fdf1beae4de2f
SOPHIE DEBENEDETTO <sophiedebenedetto@SOPHIEs-MacBook-Air.local>
2015-06-11 19:28:56 -0400

    reconfig api

--- config/initializers/grape.rb
+++ config/initializers/grape.rb
@@ -1,3 +1,3 @@
 GrapeSwaggerRails.options.url = "/api/v1/swagger_doc"
 GrapeSwaggerRails.options.app_name = "Flatbook"
-GrapeSwaggerRails.options.app_url = "http://localhost:3000"
+GrapeSwaggerRails.options.app_url = "https://flatbook-api.herokuapp.com/"
